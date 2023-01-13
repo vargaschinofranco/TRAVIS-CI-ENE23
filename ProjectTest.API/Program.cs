59247ccc588784162ebf743993f5dca3c71e5ac4
Stywar <franco111988@gmail.com>
2023-01-12 20:29:04 -0500

    new changes

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,7 +11,7 @@ var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
+    return "GET - Net 6 Running ... 12012023-v1.0.0 CI/CD in TRAVISCI AFORO255";
 });
 
 app.MapPost("/", () =>
